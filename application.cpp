bc5f56e83639e84cfb1f65d33993e4b6ae20eca3
Pranav Kulkarni <pranav.615@gmail.com>
2020-05-30 10:52:23 +0530

    Minor changes
    Removed findAllOccurances function.
    Added a few comments.

--- application.cpp
+++ application.cpp
@@ -1,9 +1,7 @@
 // sudo apt-get install libssl-dev
-// g++ application.cpp -lcrypto
+// g++ application.cpp -lcrypto -lssl
 
-
-#include<bits/stdc++.h>
-#include <fstream>
+#include <bits/stdc++.h>
 #include <openssl/pem.h>
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
@@ -13,12 +11,11 @@
 #include <string.h>
 using namespace std;
 
-
 string checkIfFileHasBeenDraggedIn(string inputString)
 {
 	string temp = inputString;
 
-	if(temp[0] != '/')// Yes, the file has been dragged and dropped into the console. contains single quote (') at both start and end which has to be removed.
+	if (temp[0] != '/') // Yes, the file has been dragged and dropped into the console. contains single quote (') at both start and end which has to be removed.
 	{
 		// remove the first and last chars
 		temp.erase(temp.begin() + 0);
@@ -27,43 +24,25 @@ string checkIfFileHasBeenDraggedIn(string inputString)
 	return temp;
 }
 
-
-
-//  Find all positions of the a subString in given string
-void findAllOccurances(vector<int> & vec, string data, string toSearch)
+string _asn1int(const ASN1_INTEGER *input) // Converts the serial number to a string
 {
-	// Get the first occurrence
-	int pos = data.find(toSearch);
- 
-	// Repeat till end is reached
-	while( pos != std::string::npos)
+	BIGNUM *bn = ASN1_INTEGER_to_BN(input, NULL);
+	if (!bn)
 	{
-		// Add position to the vector
-		vec.push_back(pos);
- 
-		// Get the next occurrence from the current position
-		pos = data.find(toSearch, pos + toSearch.size()  );
-	}
-}
-
-
-string _asn1int(const ASN1_INTEGER *input)
-{
-    BIGNUM *bn = ASN1_INTEGER_to_BN(input, NULL);
-	if(!bn) {
-		cout<<"Error converting ASN1INT to BIGNUM"<<endl;
+		cout << "Error converting ASN1INT to BIGNUM" << endl;
 		exit(-1);
 	}
 
 	char *tmp = BN_bn2hex(bn);
-	if(!bn) {
-		cout<<"Error converting BIGNUM to char*"<<endl;
+	if (!bn)
+	{
+		cout << "Error converting BIGNUM to char*" << endl;
 		BN_free(bn);
 		exit(-1);
 	}
 
 	string asn1string(tmp);
-	
+
 	//Debug
 	// cout<<"Func -> ASN1ToInt, return value = "<<asn1string<<endl;
 
@@ -71,7 +50,6 @@ string _asn1int(const ASN1_INTEGER *input)
 	BN_free(bn);
 
 	return asn1string;
-
 }
 
 string getSerialNumber(X509 *input)
@@ -86,49 +64,50 @@ string getRevokedCertSerialNumber(const X509_REVOKED *input)
 
 X509_CRL *newCRL(string CRLFileName)
 {
-  	BIO *crlbio = NULL;
+	BIO *crlbio = NULL;
 	crlbio = BIO_new(BIO_s_file());
 	if (BIO_read_filename(crlbio, CRLFileName.c_str()) <= 0)
-		cout<<"Error loading CRL into memory."<<endl;
+		cout << "Error loading CRL into memory." << endl;
 
-	X509_CRL *crl = d2i_X509_CRL_bio(crlbio,NULL); //if (format == FORMAT_PEM) crl=PEM_read_bio_X509_CRL(bio,NULL,NULL,NULL);
-	if(crl == NULL) {
-		cout<<"Error converting DER to X509_CRL"<<endl;
-      	exit(-1);
-    }
+	X509_CRL *crl = d2i_X509_CRL_bio(crlbio, NULL); //if (format == FORMAT_PEM) crl=PEM_read_bio_X509_CRL(bio,NULL,NULL,NULL);
+	if (crl == NULL)
+	{
+		cout << "Error converting DER to X509_CRL" << endl;
+		exit(-1);
+	}
 	BIO_free(crlbio);
 	return crl;
 }
 
-STACK_OF(X509)* getCertStackFromFile(string certStackFilename) {
+STACK_OF(X509) * getCertStackFromFile(string certStackFilepath)
+{
 
-	char filePath[certStackFilename.length()+1];
-	strcpy(filePath, certStackFilename.c_str());
+	char filePath[certStackFilepath.length() + 1];
+	strcpy(filePath, certStackFilepath.c_str());
 
 	SSL_CTX *sslCtx = SSL_CTX_new(SSLv23_server_method());
 	SSL_CTX_use_certificate_chain_file(sslCtx, filePath);
 
-
-	STACK_OF(X509) *tempCertStack;
-	STACK_OF(X509) *certStack;
+	STACK_OF(X509) * tempCertStack;
+	STACK_OF(X509) * certStack;
 	X509 *leaf;
 	int num;
 
-	
 	// Get the certs from sslCtx into temp_stack
-	if(SSL_CTX_get0_chain_certs(sslCtx, &tempCertStack) == 0) {
-		cout<<"Error in getting stack from SSL_CTX"<<endl;
+	if (SSL_CTX_get0_chain_certs(sslCtx, &tempCertStack) == 0)
+	{
+		cout << "Error in getting stack from SSL_CTX" << endl;
 		exit(-1);
 	}
 
 	// Print the leaf cert
 	leaf = SSL_CTX_get0_certificate(sslCtx);
 
-
 	// Create a copy of the stack
 	certStack = X509_chain_up_ref(tempCertStack); // This increases the referencability of tempCertStack by 1, and assigns it to certStack. Now, even if certStack is freed, leaf will continue to function.
-	if(certStack == NULL) {
-		cout<<"Error creating copy of stack"<<endl;
+	if (certStack == NULL)
+	{
+		cout << "Error creating copy of stack" << endl;
 		exit(-1);
 	}
 
@@ -137,40 +116,34 @@ STACK_OF(X509)* getCertStackFromFile(string certStackFilename) {
 	//Insert the leaf cert into stack
 	num = sk_X509_insert(certStack, leaf, 0);
 	// cout<<"Inserted leaf cert into stack"<<endl;
-	if(num == 0) {
-		cout<<"Error inserting leaf cert into stack"<<endl;
+	if (num == 0)
+	{
+		cout << "Error inserting leaf cert into stack" << endl;
 		exit(-1);
 	}
 	// cout<<"Number of certs in stack = "<<num<<endl;
 
-
 	SSL_CTX_free(sslCtx);
 
 	return certStack;
-
 }
-
 
 int main()
 {
 	OpenSSL_add_all_algorithms();
 	ERR_load_BIO_strings();
 
-
-
 	// Display intro message.
 
-	cout<<"\nThis is a tool to validate a given certificate chain file against a given CRL. \n\n";
-	cout<<"----------------------------------\n\n";
+	cout << "\nThis is a tool to validate a given certificate chain file against a given CRL. \n\n";
+	cout << "----------------------------------\n\n";
 
+	// Get the path of the certificate chain file
 
-
-	//get the path of the certificate chain file
-	
-	cout<<"Enter the full path of the certificate chain file. ";
-	cout<<"Alternatively, drag and drop the file into this terminal window."<<endl;
+	cout << "Enter the full path of the certificate chain file. ";
+	cout << "Alternatively, drag and drop the file into this terminal window." << endl;
 	string certChainFilePath;
-	cin>>certChainFilePath;
+	cin >> certChainFilePath;
 
 	certChainFilePath = checkIfFileHasBeenDraggedIn(certChainFilePath);
 
@@ -178,45 +151,36 @@ int main()
 
 	STACK_OF(X509) *cert_stack = getCertStackFromFile(certChainFilePath);
 	int numberOfCertificates = sk_X509_num(cert_stack);
-	
-	for(int i =0 ; i < numberOfCertificates ; i++) {
-		X509* temp = sk_X509_value(cert_stack, i);
-		chainFileSerialNumbers.push_back(getSerialNumber(temp));
-	}	
 
-
-	// -----------------------------------------------------------------
-	// -----------------------------------------------------------------
-
-	cout<<"\nThese are the serial numbers in the chain file:"<<endl; // Display all serial numbers in the chain file.
-	for(int i = 0 ; i < numberOfCertificates ; i++)
+	for (int i = 0; i < numberOfCertificates; i++)
 	{
-		cout<<(i+1)<<". "<<chainFileSerialNumbers[i]<<endl;
+		X509 *temp = sk_X509_value(cert_stack, i);
+		chainFileSerialNumbers.push_back(getSerialNumber(temp)); // Add the serial number to the chainFileSerialNumbers vector.
 	}
-	cout<<endl;
 
+	cout << "\nThese are the serial numbers in the chain file:" << endl; // Display all serial numbers in the chain file.
+	for (int i = 0; i < numberOfCertificates; i++)
+	{
+		cout << (i + 1) << ". " << chainFileSerialNumbers[i] << endl;
+	}
+	cout << endl;
 
 	// We now have all serial number in the string vector chainFileSerialNumbers.
 
 	// we can now compare these againt the serial numbers in the CRL file.
 
-	// In case the CRL is massive, it's serial numbers can be put in a hashmap for O(1) lookup.
-
-
-
-
+	// In case the CRL is massive, its serial numbers can be put in a hashmap for O(1) lookup.
 
 	// Get the CRL file path from the user.
 
-	cout<<"Enter the full path of the CRL file. ";
-	cout<<"Alternatively, drag and drop the file into this terminal window."<<endl;
+	cout << "Enter the full path of the CRL file. ";
+	cout << "Alternatively, drag and drop the file into this terminal window." << endl;
 	string CRLFilePath;
-	cin>>CRLFilePath;
+	cin >> CRLFilePath;
 
 	CRLFilePath = checkIfFileHasBeenDraggedIn(CRLFilePath);
 
-	X509_CRL *CRLFileInX509  = newCRL(CRLFilePath);
-
+	X509_CRL *CRLFileInX509 = newCRL(CRLFilePath);
 
 	// Get the number of revoked certificates from the CRL.
 	STACK_OF(X509_REVOKED) *revokedStack = NULL;
@@ -224,14 +188,12 @@ int main()
 
 	int numberOfRevokedCeritficates = sk_X509_REVOKED_num(revokedStack);
 
-
-
 	// Extract serial numbers of all revoked certificates.
 
 	vector<string> revokedSerialNumbers;
 	X509_REVOKED *revEntry = NULL;
 
-	for(int i = 0 ; i < numberOfRevokedCeritficates ; i++)
+	for (int i = 0; i < numberOfRevokedCeritficates; i++)
 	{
 		revEntry = sk_X509_REVOKED_value(revokedStack, i);
 		revokedSerialNumbers.push_back(getRevokedCertSerialNumber(revEntry)); // Add it to the revokedSerialNumbers vector.
@@ -246,28 +208,27 @@ int main()
 	cout<<endl;
 	*/
 
-
-	// Now we have 2 vectors: chainFileSerialNumbers and revokedSerialNumbers.
+	// Now we have 2 vectors: chainFileSerialNumbers and revokedSerialNumbers, with the required serial numbers.
 
 	// Do the checking => See if there is any cert from the chain file which is listed in the CRL. If there is, the chain file is NOT VALID.
 
-	for(int i=0 ; i<numberOfCertificates ; i++)
+	for (int i = 0; i < numberOfCertificates; i++)
 	{
 		string toBeChecked = chainFileSerialNumbers[i]; // This cert's serial number (from the chain file) will be checked against all the CRT serial numbers.
 
-		for(int j=0 ; j<numberOfRevokedCeritficates ; j++)
+		for (int j = 0; j < numberOfRevokedCeritficates; j++)
 		{
-			if(toBeChecked == revokedSerialNumbers[j])
+			if (toBeChecked == revokedSerialNumbers[j])
 			{
-				cout<<"\n"<<toBeChecked<<" has been revoked. INVALID CHAIN. index = "<<j+1<<endl;
+				cout << "\n"
+					 << toBeChecked << " has been revoked. INVALID CHAIN. index = " << j + 1 << endl;
 				exit(1);
 			}
 		}
 	}
 
-
 	// If we reach till here, none of the chain file's certs have been revoked. It is a VALID CHAIN.
-	cout<<"None of the certificates in the chain have been revoked. This is a VALID CHAIN."<<endl;
+	cout << "None of the certificates in the chain have been revoked. This is a VALID CHAIN." << endl;
 
 	return 0;
 }
