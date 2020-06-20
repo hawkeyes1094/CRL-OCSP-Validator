abeb08d4d03c45005f90449645e183a17bf9b819
Pranav Kulkarni <pranav.615@gmail.com>
2020-06-20 19:38:28 +0530

    Added feature to use both DER and PEM encoded CRL files.
    Earlier, we were only handling DERs.

--- working_integrated_code/CRLFunctions.cpp
+++ working_integrated_code/CRLFunctions.cpp
@@ -16,7 +16,7 @@ These function definations in this file:
 
 using namespace std;
 
-X509_CRL *getNewCRLFromPath(string CRLFilePath)
+/*X509_CRL *getNewCRLFromPath(string CRLFilePath)
 {
     BIO *newCRLbio = NULL;
 
@@ -33,6 +33,62 @@ X509_CRL *getNewCRLFromPath(string CRLFilePath)
         cout << "Error converting DER to X509_CRL" << endl;
         exit(-1);
     }
+    BIO_free(newCRLbio);
+    return newCRL;
+}*/
+
+X509_CRL *getNewCRLFromPath(string CRLFilePath)
+{
+    std::cout<< "hello\n";
+    BIO *newCRLbio = NULL;
+
+    newCRLbio = BIO_new(BIO_s_file());
+
+    if (BIO_read_filename(newCRLbio, CRLFilePath.c_str()) <= 0) // Load the file (from the path) into the new BIO.
+    {
+        cout << "Error loading CRL into memory." << endl;
+    }
+
+    // It is possible for the CRL to be encoded in DER or PEM formats.
+    // We need to check which one it is.
+    // We use the fact DER encoded files always begin with byte 0x30 (aka char '0' in ASCII) and PEM encoded files begin with "----".
+
+    char *firstByte = (char*)malloc(sizeof(char)); // Because the sizeof(char) = 1 byte.
+    size_t bytesRead;
+    if (BIO_read_ex(newCRLbio, firstByte, 1, &bytesRead) == 0) // Reads the first byte into firstByte.
+    {
+        std::cerr << "Error reading the first byte from the CRL file." << std::endl;
+        exit(-1);
+    }
+
+    if (BIO_seek(newCRLbio, 0) == -1) // To reset the file BIO to the beginning of the file.
+    {
+        std::cerr << "Error resetting the file BIO to the beginnning of the file" << std::endl;
+        exit(-1);
+    }
+
+    X509_CRL *newCRL = NULL;
+    if (firstByte[0] == '\x30') //It is encoded using DER.
+    {
+        newCRL = d2i_X509_CRL_bio(newCRLbio, NULL);
+
+        if (newCRL == NULL)
+        {
+            cout << "Error reading the DER encoded CRL file." << endl;
+            exit(-1);
+        }
+    }
+    else // It is encoded using PEM.
+    {
+        newCRL = PEM_read_bio_X509_CRL(newCRLbio, NULL, NULL, NULL);
+
+        if (newCRL == NULL)
+        {
+            cout << "Error reading the PEM encoded CRL file." << endl;
+            exit(-1);
+        }
+    }
+
     BIO_free(newCRLbio);
     return newCRL;
 }
