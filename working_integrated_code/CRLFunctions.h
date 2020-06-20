73625e7a445cc21cc31cf384bcdba518cc14d61f
Pranav Kulkarni <pranav.615@gmail.com>
2020-06-20 17:32:15 +0530

    Readability

--- working_integrated_code/CRLFunctions.h
+++ working_integrated_code/CRLFunctions.h
@@ -10,7 +10,6 @@ Functions used for the CRL file.
 X509_CRL * getNewCRLFromPath(std::string CRLFilePath);
 
 
-
 std::string getRevokedSerialNumberFromX509(const X509_REVOKED *input);
 
 void printCRLSerialNumbers(std::map<std::string, int> revokedSerialNumbers);
