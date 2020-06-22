c59da7c38b12d852988141d7807e8d8dec2013a9
Teja Juluru <44445080+hawkeyes1094@users.noreply.github.com>
2020-06-22 19:51:53 +0530

    Removed parseURL() function

--- include/OCSPFunctions.h
+++ include/OCSPFunctions.h
@@ -6,8 +6,6 @@ OCSP_CERTID *getCertificateID(X509 *thisCert, X509 *issuerCert);
 
 OCSP_REQUEST *createOCSPRequest(OCSP_CERTID *certID, std::string ocspURL);
 
-void parseURL(char *ocspURL, char **host, char **port, char **path, int *useSSL);
-
 OCSP_REQ_CTX *createOCSPRequestCTX(BIO *connBIO, char *path, char *host);
 
 void getCertificateStatus(OCSP_RESPONSE *response, OCSP_CERTID *certID, int *status, int *reason, ASN1_GENERALIZEDTIME **revokedTime);
