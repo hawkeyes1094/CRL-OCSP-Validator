78fd5914de83ac59bc65374a1648f60cbd6d520a
Pranav Kulkarni <pranav.615@gmail.com>
2020-06-23 09:19:46 +0530

    Update test_correctCertStackOrder.cpp

--- test/unit_testing/test_correctCertStackOrder/test_correctCertStackOrder.cpp
+++ test/unit_testing/test_correctCertStackOrder/test_correctCertStackOrder.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 
 /*
+
 g++ -c -o Common.o Common.cpp && \
 g++ -c -o ChainFileFunctions.o ChainFileFunctions.cpp && \
 g++ -c -o test_correctCertStackOrder.o test_correctCertStackOrder.cpp && \
 g++ -o test_correctCertStackOrder Common.o ChainFileFunctions.o test_correctCertStackOrder.o -lcrypto -lssl && \
 ./test_correctCertStackOrder
-
 
 */
 
@@ -31,7 +31,7 @@ vector<string> getVectorOfSerialNumbers(STACK_OF(X509) *certStack)
     for (int i = 0; i < numberOfCertificatesInChain; i++)
     {
         X509 *thisCert = sk_X509_value(certStack, i);                        // Pick one cert from the stack.
-        output.push_back(getSerialNumberFromX509(thisCert)); // Add the serial number to the functionOutputSerialNumbers vector.
+        output.push_back(getSerialNumberFromX509(thisCert)); // Add the serial number to the output vector.
     }
 
     return output;
@@ -84,7 +84,7 @@ int main()
     // We will be testing 2 cases:
     // 1. order is correct. that is, leaf -> intermediat(es) -> root
     // 2. order is reversed. that is, root -> intermediat(es) -> leaf
-    // in case of jumbled we terminate the program, so we wont be testing it here.
+    // i=In case of jumbled we terminate the program, so we wont be testing it here.
 
     cout << "\n\nUnit testing of the funciton 'correctCertStackOrder': "<< endl;
     if (correctOrder() == 1)
